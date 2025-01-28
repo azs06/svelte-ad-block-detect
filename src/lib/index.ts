ae2ba1124c825a6127b47bed40ecd91970de081b
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-29 02:49:14 +0600

    export adBlockEnabled from index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { detectAdBlock } from "$lib/adblock";
+import { adBlockEnabled } from "$lib/stores/adblock.store";
 
-export { detectAdBlock };
+export { detectAdBlock, adBlockEnabled };
